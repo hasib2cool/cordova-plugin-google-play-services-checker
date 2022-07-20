69f752f2f899877c95ac3a4a62839ce1f13125a9
hasib2cool <39165424+hasib2cool@users.noreply.github.com>
2022-07-20 16:34:56 +0400

    Update GooglePlayServicesChecker.java

--- src/android/com/dinamexoft/googleplayserviceschecker/GooglePlayServicesChecker.java
+++ src/android/com/dinamexoft/googleplayserviceschecker/GooglePlayServicesChecker.java
@@ -21,9 +21,17 @@ public class GooglePlayServicesChecker extends CordovaPlugin {
             final int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
             GoogleApiAvailability googleAPI = GoogleApiAvailability.getInstance();
             int result = googleAPI.isGooglePlayServicesAvailable(this.cordova.getContext());
-            
-                json.put("status", result);
-           
+            if (result != ConnectionResult.SUCCESS) {
+                Log.i(APPTAG, "result != ConnectionResult.SUCCESS");
+                json.put("status", false).put("ConnectionResult", result);
+                if (googleAPI.isUserResolvableError(result)) {
+                    Log.i(APPTAG, "googleAPI.isUserResolvableError(result)");
+                    //googleAPI.getErrorDialog(this.cordova.getActivity(), result, PLAY_SERVICES_RESOLUTION_REQUEST).show();
+                    json.put("message", "user action required");
+                }
+            } else {
+                json.put("status", true);
+            }
             callbackContext.success(json);
             return true;
         } else {
